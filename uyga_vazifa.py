98b36ffb63285336520c5005e4c8df7faa66eb8a
Farxod <qayumovfarxod697803@gmail.com>
2024-07-07 02:05:47 +0500

    tub sonlar yasaldi

--- uyga_vazifa.py
+++ uyga_vazifa.py
@@ -1,57 +1,56 @@
-# class Fibonacci:
-#     def __init__(self, stop,first , secend ):
+# class Tub_sonlar:
+#     def __init__(self, start: int, stop: int ):
+#         self.start = start
 #         self.stop = stop
-#         self.first = first
-#         self.secend = secend
-        
 
 #     def __iter__(self):
 #         return self
 
 #     def __next__(self):
         
-       
-#         if self.stop<self.first:
+#         if self.start >= self.stop:
 #             raise StopIteration
 #         else:
-#             temp = self.first
-#             self.first = self.secend
-#             self.secend +=temp
-#             return temp
+#             check = 0
+#             for n in range(2 ,self.start):
+#                 if self.start%n ==0:
+#                     check+=1
+#             if check>0:
+#                 self.start +=1
+#             else:
+#                 temp = self.start
+#                 self.start+=1
+#                 return temp
+        
 
 
-
-    
-
-
-# my_fibonacci = Fibonacci(100,0,1)
-
-# for i in my_fibonacci:
+# my_number = Tub_sonlar(6,20)
+# for i in my_number:
+#     if i ==None:
+#         continue
 #     print(i)
 
 
-class Fibonacci:
-    def __init__(self, start, stop, secend):
-        self.start = start
-        self.stop = stop
-        self.secend = secend
+def Tub_son(stop):
+    number = 2
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        
-        if self.start >= self.stop:
-            raise StopIteration
+    while True:
+        check = 0
+        for n in range(1 ,number):
+            if number%n==0:
+                check+=1
+        if number>=stop:
+            break
         else:
-            temp = self.start
-            self.start = self.secend
-            self.secend +=temp
-            return temp
-        
+            if check>=2:
+                number+=1
+                continue
+            else:
+                yield number
+                number+=1
+            
 
-my_fibonachi = Fibonacci(0 , 100 , 1)
-for i in my_fibonachi:
+
+for i in Tub_son(130):
     print(i)
-
        
